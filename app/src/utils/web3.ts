5604f23a4a5126894c31dd969a947e2b0903a534
Steven Ciraolo <steven.ciraolo@gmail.com>
2020-10-19 09:14:51 -0700

    updated contract organization

--- app/src/utils/web3.ts
+++ app/src/utils/web3.ts
@@ -1,108 +1,11 @@
-import { Eth } from 'web3x-es/eth'
-import { WebsocketProvider, HttpProvider } from 'web3x-es/providers';
-import Fortmatic from 'fortmatic'
-import Web3 from 'web3'
-import { Algernon } from '../generated/Algernon';
-import { AlgerToken } from '../generated/AlgerToken';
-import { ethers } from 'ethers';
-import initIpfs from '../fileStorage/ipfs'
-
-declare const window : any
-
-// "https://dev-testnet-v1-0.skalelabs.com"
-
-const AlgerTokenArtifact = require("../contracts/AlgerToken.json")
-const AlgernonArtifact = require("../contracts/Algernon.json")
-
-const chainEndpoint = process.env.REACT_APP_LOCAL_CHAIN_ENDPOINT
-
-const ipfsEndpoint = process.env.IPFS_ENDPOINT || '/ip4/0.0.0.0/tcp/5001'
-
-const networkId = '1587695479278'
-console.log(process.env)
+const AlgernonArtifact = require("../abis/Algernon.json")
 
 const getLatestDeployment = (deployments: any) => {
   const key = Object.keys(deployments).sort((a, b) => parseInt(a) - parseInt(b)).pop()
   return deployments[key!]
 }
 
-
-// const wsProvider = new WebsocketProvider(schainEndpoint);
-
-
-export const fm = new Fortmatic(process.env.REACT_APP_FORTMATIC_API_KEY!, {rpcUrl: chainEndpoint!, chainId: 1337});
-
-const httppProvider = new HttpProvider(chainEndpoint!)
-
-const web3Eth = new Eth(httppProvider)
-
-export const ipfs = initIpfs(ipfsEndpoint)
-
-
-const getWeb3 = () => {
-  if (true){//window.ethereum) {
-    // @ts-ignore
-    const web3 = new Web3(fm.getProvider())//window.ethereum)
-    return web3
-    try {
-      // Request account access if needed
-      window.ethereum.enable();
-      // Acccounts now exposed
-      return web3;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  // Legacy dapp browsers...
-  else if (window.web3) {
-    // Use Mist/MetaMask's provider.
-    const web3 = window.web3;
-    console.log('Injected web3 detected.');
-    return web3;
-  }
-  // Fallback to localhost; use dev console port by default...
-  else {
-    const provider = new Web3.providers.HttpProvider('http://167.172.118.124:10003');
-    const web3 = new Web3(provider);
-    console.log('No web3 instance injected, using Local web3.');
-    return web3;
-  }
-}
-
-
-export const windowWeb3 = window.web3
-// export const web3 = getWeb3()
-
-/*
-const getChainId = async () => {
-  const id = await web3.eth.getChainId()
-  console.log(`chain id ${id}`)
-}
-
-getChainId()
-*/
-export const ethersProvider = new ethers.providers.JsonRpcProvider(chainEndpoint!)
-
-const getChainCode = async () => {
-  try {
-    const code = await ethersProvider.getCode(chainEndpoint!)
-    console.log('got code ethers ', code)
-  } catch (e) {
-    console.log('error getting ethers code ', e)
-  }
-
-  try {
-    const code = await web3Eth.getCode(AlgerTokenArtifact.networks[networkId].address)
-    console.log('got code web3 ', code)
-  } catch (e) {
-    console.log('error getting web3 code')
-  }
-
-}
-
-// getChainCode()
-
-const algernonAddress = getLatestDeployment(AlgernonArtifact.networks).address//[networkId].address;
+const algernonAddress = '0x26950f5d5B2eA2AC11d8A2cB67a62D8831Be3d8F'//getLatestDeployment(AlgernonArtifact.networks).address//[networkId].address;
 
 const getDeployedContract = (web3: any, abi: any, address: string) => {
   const contract =  new web3.eth.Contract(abi, address)
@@ -113,17 +16,4 @@ const getDeployedContract = (web3: any, abi: any, address: string) => {
 
 export const getAlgernonInstance = (web3: any) => getDeployedContract(web3, AlgernonArtifact.abi, algernonAddress)
 
-export const ethersAlgernon = new ethers.Contract(
-  AlgernonArtifact.networks[networkId].address,
-  AlgernonArtifact.abi,
-  ethersProvider
-  ) 
 
-const algerTokenAddress = AlgerTokenArtifact.networks[networkId].address;
-
-/*
-export const algerTokenContract = new AlgerToken(
-  web3, 
-  algerTokenAddress
-)
-*/
--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -5,7 +5,7 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "contracts"),
+  contracts_build_directory: path.join(__dirname, "build"),
   networks: {
     development: {
       host: "0.0.0.0",
