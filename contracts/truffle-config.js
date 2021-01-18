4d5356cbbdf9ccf56dabd0409be4a905ad2a7e75
Steven Ciraolo <steven.ciraolo@gmail.com>
2021-01-17 19:56:51 -0800

    added kovan deployment config

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -1,31 +1,41 @@
 require('dotenv').config()
 const path = require("path");
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const kovanWalletArgs = {
+  privateKeys: [process.env.PRIVATE_KEY],
+  providerOrUrl: process.env.KOVAN_ENDPOINT
+}
+
+const skaleWalletArgs = {
+  privateKeys: [process.env.SKALE_PRIVATE_KEY],
+  providerOrUrl: process.env.SKALE_ENDPOINT
+}
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "build"),
+  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
-    development: {
+    develop: {
       host: "0.0.0.0",
       port: 8545,
       network_id: "*" // Match any network id
     },
     skale: {
-      provider: () => new HDWalletProvider(process.env.SKALE_PRIVATE_KEY, process.env.SKALE_ENDPOINT),
+      provider: () => new HDWalletProvider(skaleWalletArgs),
       gasPrice: 0,
       network_id: process.env.SKALE_NETWORK_ID
     },
-    rinkeby: {
-      provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, process.env.RINKEBY_ENDPONIT),
-      network_id: 4,
-      gas: 4612388 // Gas limit used for deploys
+    kovan: {
+      provider: () => new HDWalletProvider(kovanWalletArgs),
+      network_id: 42,
+      gas: 12400000 // Gas limit used for deploys
     }
   },
   compilers: {
     solc: {
-      version: "0.6.12"
+      version: "0.6.2"
     }
   }
 };
