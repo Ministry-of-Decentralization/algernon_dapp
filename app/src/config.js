26a325577bc4df0436bfdcc23dc4cac5b7efd0f5
Steven Ciraolo <steven.ciraolo@gmail.com>
2021-03-07 19:37:30 -0800

    added subgraph templating

--- app/src/config.js
+++ app/src/config.js
@@ -18,8 +18,8 @@ const ENVS = {
     chainId: 42,
     networkName: 'Kovan',
     chainEndpoint: process.env.REACT_APP_KOVAN_CHAIN_ENDPOINT,
-    algernonAddress: '0x1acE7049142881d42Fe104B212C8E7a457eE0C91',
-    algerTokenAddress: '0x98012ed413Bc79B2A2fc1420FDC90670aAb96B61',
+    algernonAddress: '0xE494E00411D49cfb6466391c5aA0166505fEe6B7',
+    algerTokenAddress: '0xDC904045490Fd90415B3299373A4741B15393fD5',
     theGraphEndpoint: process.env.REACT_APP_KOVAN_SUBGRAPH_ENDPOINT,
     ipfsEndpoint: process.env.REACT_APP_KOVAN_IPFS_ENDPOINT,
     fortmaticApiKey: process.env.REACT_APP_FORTMATIC_API_TEST_KEY
